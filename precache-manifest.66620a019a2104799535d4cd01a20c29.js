b9ca3bc929c86c7068836631130d341f1db855b9
Anton Shumeiko <49491454+strang999@users.noreply.github.com>
2020-07-11 01:31:22 +0300

    UPL

---
+++ precache-manifest.66620a019a2104799535d4cd01a20c29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43e9b65f113f7522ffba3cdd8646ca9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4a0f4effb50cdaaccb5",
+    "url": "/static/css/main.af61bf4e.chunk.css"
+  },
+  {
+    "revision": "dc3b4e4a0f829b134385",
+    "url": "/static/js/2.377c2867.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.377c2867.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4a0f4effb50cdaaccb5",
+    "url": "/static/js/main.eb13753e.chunk.js"
+  },
+  {
+    "revision": "771db5c0ceae6ff6d487",
+    "url": "/static/js/runtime-main.b48f82dd.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/logo.b8503d26.png"
+  }
+]);
